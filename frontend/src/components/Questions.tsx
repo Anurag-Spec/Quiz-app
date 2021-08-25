3b96d3a10ec282647b62aec9e49b684f88c8d015
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-26 00:25:27 +0530

    created function for questions

--- frontend/src/components/Questions.tsx
+++ frontend/src/components/Questions.tsx
@@ -1,5 +1,37 @@
 import React from "react";
 
-const Question = () => <div>Questions</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const Question: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default Question;
