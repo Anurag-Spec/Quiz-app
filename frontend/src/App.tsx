99d692e7887da48948b416486da5633ee381b2bb
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-08-26 09:43:01 +0530

    fixed types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { fetchQuestions } from "./API";
 import Questions from "./components/Questions";
 import { QuestionState, Difficulty } from "./API";
 
-type AnswerObject = {
+export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
