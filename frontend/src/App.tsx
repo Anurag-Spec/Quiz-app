083ad5a7f0577cb9b7629b7fa69c9b5dbd11b700
Anurag-Spec <79415746+Anurag-Spec@users.noreply.github.com>
2021-10-09 19:58:40 +0530

    added popup login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchQuestions } from "./API";
 import Questions from "./components/Questions";
 import { QuestionState, Difficulty } from "./API";
 import { GlobalStyle, Wrapper } from "./App.styles";
+import firebase from "firebase/compat/app";
+import firebaseui from "firebaseui";
 
 export type AnswerObject = {
   question: string;
@@ -11,14 +13,40 @@ export type AnswerObject = {
   correctAnswer: string;
 };
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDhqSLqvBhdlNjTV1tDUmNIXAkKJ1pdPio",
+  authDomain: "quiz-app-23324.firebaseapp.com",
+});
+
 const TOTAL_QUESTIONS = 10;
 function App() {
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function (
+        authResult: any,
+        redirectUrl: any
+      ) {
+        return true;
+      },
+      uiShown: function () {},
+    },
+    signInFlow: "popup",
+    signInSuccessUrl: "<url-to-redirect-to-on-success>",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+  };
+
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+  const [isSignedIn, setisSignedIn] = useState(false);
 
   const startQuiz = async (category: any) => {
     setLoading(true);
@@ -52,6 +80,12 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setisSignedIn(!!user);
+    });
+  }, [isSignedIn]);
+
   const nextQuestion = () => {
     const nextQuestion = number + 1;
     if (nextQuestion === TOTAL_QUESTIONS) {
@@ -61,64 +95,70 @@ function App() {
 
   return (
     <>
-      <GlobalStyle />
-      <Wrapper>
-        <h1> The Quiz </h1>
-        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-          <>
-            <h3>Choose a Category to start Playing</h3>
-            <button
-              value="31"
-              className="start"
-              onClick={(e) => startQuiz(e.currentTarget.value)}
-            >
-              Anime
-            </button>
-            <button
-              value="9"
-              className="start"
-              onClick={(e) => startQuiz(e.currentTarget.value)}
-            >
-              General Knowledge
-            </button>
-            <button
-              value="10"
-              className="start"
-              onClick={(e) => startQuiz(e.currentTarget.value)}
-            >
-              Films
-            </button>
-            <button
-              value="18"
-              className="start"
-              onClick={(e) => startQuiz(e.currentTarget.value)}
-            >
-              Computers
-            </button>
-          </>
-        ) : null}
+      {isSignedIn ? (
+        <>
+          <GlobalStyle />
+          <Wrapper>
+            <h1> The Quiz </h1>
+            {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+              <>
+                <h3>Choose a Category to start Playing</h3>
+                <button
+                  value="31"
+                  className="start"
+                  onClick={(e) => startQuiz(e.currentTarget.value)}
+                >
+                  Anime
+                </button>
+                <button
+                  value="9"
+                  className="start"
+                  onClick={(e) => startQuiz(e.currentTarget.value)}
+                >
+                  General Knowledge
+                </button>
+                <button
+                  value="10"
+                  className="start"
+                  onClick={(e) => startQuiz(e.currentTarget.value)}
+                >
+                  Films
+                </button>
+                <button
+                  value="18"
+                  className="start"
+                  onClick={(e) => startQuiz(e.currentTarget.value)}
+                >
+                  Computers
+                </button>
+              </>
+            ) : null}
 
-        {!gameOver ? <p className="score"> Score: {score}</p> : null}
-        {loading && <p>Loading Questions ....</p>}
-        {!loading && !gameOver && (
-          <Questions
-            questionNr={number + 1}
-            totalQuestions={TOTAL_QUESTIONS}
-            question={questions[number].question}
-            answers={questions[number].answers}
-            userAnswer={userAnswers ? userAnswers[number] : undefined}
-            callback={checkAnswer}
-          />
-        )}
-        {!gameOver &&
-        !loading &&
-        userAnswers.length === number + 1 &&
-        number !== TOTAL_QUESTIONS - 1 ? (
-          <button className="next" onClick={nextQuestion}>
-            Next Question
-          </button>
-        ) : null}
-      </Wrapper>
+            {!gameOver ? <p className="score"> Score: {score}</p> : null}
+            {loading && <p>Loading Questions ....</p>}
+            {!loading && !gameOver && (
+              <Questions
+                questionNr={number + 1}
+                totalQuestions={TOTAL_QUESTIONS}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswers ? userAnswers[number] : undefined}
+                callback={checkAnswer}
+              />
+            )}
+            {!gameOver &&
+            !loading &&
+            userAnswers.length === number + 1 &&
+            number !== TOTAL_QUESTIONS - 1 ? (
+              <button className="next" onClick={nextQuestion}>
+                Next Question
+              </button>
+            ) : null}
+          </Wrapper>
+        </>
+      ) : (
+        ui.start("#firebaseui-auth-container", uiConfig)
+      )}
     </>
   );
 }
